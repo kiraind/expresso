265d34f5cba4196f918d8dfe9e1d7593b016224e
Alexei Paksevatkin <ne.eshte@otso.city>
2020-03-11 21:45:57 +0300

    aaa

--- index.js
+++ index.js
@@ -42,45 +42,27 @@ http.createServer(function (request, response) {
         return
     }
 
-    let filename = path.join(process.cwd(), 'static', uri)
+    const filename = path.join(process.cwd(), 'static', '/index.html')
 
-    const contentTypesByExtension = {
-        '.html': 'text/html',
-        '.css': 'text/css',
-        '.js': 'text/javascript'
-    }
-
-    fs.exists(filename, function (exists) {
-        if (!exists) {
-            response.writeHead(404, { 'Content-Type': 'text/plain' })
-            response.write('404 Not Found\n')
+    fs.readFile(filename, 'binary', function (err, file) {
+        if (err) {
+            response.writeHead(500, { 'Content-Type': 'text/plain' })
+            response.write(err + '\n')
             response.end()
             return
         }
 
-        if (fs.statSync(filename).isDirectory()) filename += '/index.html'
+        const headers = {}
 
-        fs.readFile(filename, 'binary', function (err, file) {
-            if (err) {
-                response.writeHead(500, { 'Content-Type': 'text/plain' })
-                response.write(err + '\n')
-                response.end()
-                return
-            }
-
-            const headers = {}
-            const contentType = contentTypesByExtension[path.extname(filename)]
-            if (contentType) {
-                headers['Content-Type'] = contentType
-            }
-            response.writeHead(200, headers)
-            response.write(file, 'binary')
-            response.end()
-        })
+        headers['Content-Type'] = 'text/html'
+        
+        response.writeHead(200, headers)
+        response.write(file, 'binary')
+        response.end()
     })
 }).listen(port)
 
-console.log('Static file server running at\n  => http://localhost:' + port)
+console.log('Static file server running at\n  => http://localhost:' + port + '/expresso')
 
 function makeResponse(rawInput) {
     const expression = new Node( rawInput )
